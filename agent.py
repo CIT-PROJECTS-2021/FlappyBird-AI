6124563f32e8dcb3f0eba35a2faf2ca58aabf2e6
kallyas <kallyasmedia@gmail.com>
2022-01-17 20:14:12 +0300

    refactor code

--- agent.py
+++ agent.py
@@ -1,18 +1,5 @@
-import os
-import sys
-import random
-import time
-
-import cv2
-import numpy as np
-import torch
-import torch.nn as nn
-import torch.optim as optim
-
-from game.flappy import GameState
-
-
 class NeuralNetwork(nn.Module):
+
     def __init__(self):
         super(NeuralNetwork, self).__init__()
 
@@ -20,7 +7,7 @@ class NeuralNetwork(nn.Module):
         self.gamma = 0.99
         self.final_epsilon = 0.0001
         self.initial_epsilon = 0.1
-        self.number_of_iterations = 1000000
+        self.number_of_iterations = 2000000
         self.replay_memory_size = 10000
         self.minibatch_size = 32
 
@@ -35,17 +22,18 @@ class NeuralNetwork(nn.Module):
         self.fc5 = nn.Linear(512, self.number_of_actions)
 
     def forward(self, x):
-        x = self.conv1(x)
-        x = self.relu1(x)
-        x = self.conv2(x)
-        x = self.relu2(x)
-        x = self.conv3(x)
-        x = self.relu3(x)
-        x = x.view(x.size(0), -1)
-        x = self.fc4(x)
-        x = self.relu4(x)
-        x = self.fc5(x)
-        return x
+        out = self.conv1(x)
+        out = self.relu1(out)
+        out = self.conv2(out)
+        out = self.relu2(out)
+        out = self.conv3(out)
+        out = self.relu3(out)
+        out = out.view(out.size()[0], -1)
+        out = self.fc4(out)
+        out = self.relu4(out)
+        out = self.fc5(out)
+
+        return out
 
 
 def init_weights(m):
@@ -54,19 +42,18 @@ def init_weights(m):
         m.bias.data.fill_(0.01)
 
 
-def image_to_tensor(self, image):
-    image = cv2.resize(image, (84, 84))
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    image = image.reshape(1, 4, 84, 84)
-    image = torch.from_numpy(image)
-    image = image.type(torch.FloatTensor)
-    return image
+def image_to_tensor(image):
+    image_tensor = image.transpose(2, 0, 1)
+    image_tensor = image_tensor.astype(np.float32)
+    image_tensor = torch.from_numpy(image_tensor)
+    if torch.cuda.is_available():  # put on GPU if CUDA is available
+        image_tensor = image_tensor.cuda()
+    return image_tensor
 
 
-def resize_and_bgr2gray(self, image):
+def resize_and_bgr2gray(image):
     image = image[0:288, 0:404]
-    image_resize = cv2.resize(image, (84, 84))
-    image_data = cv2.cvtColor(image_resize, cv2.COLOR_BGR2GRAY)
+    image_data = cv2.cvtColor(cv2.resize(image, (84, 84)), cv2.COLOR_BGR2GRAY)
     image_data[image_data > 0] = 255
     image_data = np.reshape(image_data, (84, 84, 1))
     return image_data
@@ -74,7 +61,7 @@ def resize_and_bgr2gray(self, image):
 
 def train(model, start):
     # define Adam optimizer
-    optmizer = optim.Adam(model.parameters(), lr=1e-6)
+    optimizer = optim.Adam(model.parameters(), lr=1e-6)
 
     # initialize mean squared error loss
     criterion = nn.MSELoss()
@@ -106,17 +93,18 @@ def train(model, start):
 
         # initialize action
         action = torch.zeros([model.number_of_actions], dtype=torch.float32)
-        if torch.cuda.is_available():
+        if torch.cuda.is_available():  # put on GPU if CUDA is available
             action = action.cuda()
 
         # epsilon greedy exploration
         random_action = random.random() <= epsilon
         if random_action:
-            print('Performed random action')
-            action_index = [torch.randint(model.number_of_actions, torch.Size([]), dtype=torch.int) if random_action else torch.argmax(output)][0]
+            print("Performed random action!")
+        action_index = [torch.randint(model.number_of_actions, torch.Size([]), dtype=torch.int)
+                        if random_action
+                        else torch.argmax(output)][0]
 
-
-        if torch.cuda.is_available():
+        if torch.cuda.is_available():  # put on GPU if CUDA is available
             action_index = action_index.cuda()
 
         action[action_index] = 1
@@ -149,26 +137,25 @@ def train(model, start):
         reward_batch = torch.cat(tuple(d[2] for d in minibatch))
         state_1_batch = torch.cat(tuple(d[3] for d in minibatch))
 
-
-        if torch.cuda.is_available(): # use GPU if cuda is available
-              state_batch = state_batch.cuda()
-              action_batch = action_batch.cuda()
-              reward_batch = reward_batch.cuda()
-              state_1_batch = state_1_batch.cuda()
+        if torch.cuda.is_available():  # put on GPU if CUDA is available
+            state_batch = state_batch.cuda()
+            action_batch = action_batch.cuda()
+            reward_batch = reward_batch.cuda()
+            state_1_batch = state_1_batch.cuda()
 
         # get output for the next state
         output_1_batch = model(state_1_batch)
 
-        # set y_j to r_j for terminal state, otherwise to to r_j + gamma*max(Q)
+        # set y_j to r_j for terminal state, otherwise to r_j + gamma*max(Q)
         y_batch = torch.cat(tuple(reward_batch[i] if minibatch[i][4]
-                                    else reward_batch[i] + model.gamma * torch.max(output_1_batch[i])
-                                    for i in range(len(minibatch))))
+                                  else reward_batch[i] + model.gamma * torch.max(output_1_batch[i])
+                                  for i in range(len(minibatch))))
 
         # extract Q-value
         q_value = torch.sum(model(state_batch) * action_batch, dim=1)
 
         # PyTorch accumulates gradients by default, so they need to be reset in each pass
-        optmizer.zero_grad()
+        optimizer.zero_grad()
 
         # returns a new Tensor, detached from the current graph, the result will never require gradient
         y_batch = y_batch.detach()
@@ -178,19 +165,18 @@ def train(model, start):
 
         # do backward pass
         loss.backward()
-        optmizer.step()
+        optimizer.step()
 
         # set state to be state_1
         state = state_1
         iteration += 1
 
         if iteration % 25000 == 0:
-            torch.save(model, 'pretrained_models/current_model_' + str(iteration) + '.pth')
+            torch.save(model, "pretrained_model/current_model_" + str(iteration) + ".pth")
 
-        print('Iteration: {} | Loss: {:.4f} | epsilon: {:.2f}'.format(iteration, loss.item(), epsilon))
-        print('Iteration:', iteration, 'elapsed time:', time.time() - start, 'seconds', 'epsilon:', epsilon, 
-                'action:', action_index.cpu().detach().numpy(), 'reward: ', reward.numpy()[0][0],
-                'Q max: ', np.max(output.cpu().detach().numpy()))
+        print("iteration:", iteration, "elapsed time:", time.time() - start, "epsilon:", epsilon, "action:",
+              action_index.cpu().detach().numpy(), "reward:", reward.numpy()[0][0], "Q max:",
+              np.max(output.cpu().detach().numpy()))
 
 
 def test(model):
@@ -204,18 +190,17 @@ def test(model):
     image_data = image_to_tensor(image_data)
     state = torch.cat((image_data, image_data, image_data, image_data)).unsqueeze(0)
 
-
     while True:
         # get output from the neural network
         output = model(state)[0]
 
         action = torch.zeros([model.number_of_actions], dtype=torch.float32)
-        if torch.cuda.is_available(): # use GPU if cuda is available
+        if torch.cuda.is_available():  # put on GPU if CUDA is available
             action = action.cuda()
 
         # get action
-        action_index = action[torch.argmax(output)]
-        if torch.cuda.is_available(): # use GPU if cuda is available
+        action_index = torch.argmax(output)
+        if torch.cuda.is_available():  # put on GPU if CUDA is available
             action_index = action_index.cuda()
         action[action_index] = 1
 
@@ -224,8 +209,7 @@ def test(model):
         image_data_1 = resize_and_bgr2gray(image_data_1)
         image_data_1 = image_to_tensor(image_data_1)
         state_1 = torch.cat((state.squeeze(0)[1:, :, :], image_data_1)).unsqueeze(0)
-        
-        
+
         # set state to be state_1
         state = state_1
 
@@ -239,17 +223,18 @@ def main(mode):
             map_location='cpu' if not cuda_is_available else None
         ).eval()
 
-        if cuda_is_available: # use GPU if cuda is available
+        if cuda_is_available:  # put on GPU if CUDA is available
             model = model.cuda()
 
         test(model)
+
     elif mode == 'train':
         if not os.path.exists('pretrained_model/'):
-            os.makedirs('pretrained_model/')
+            os.mkdir('pretrained_model/')
 
         model = NeuralNetwork()
 
-        if cuda_is_available: # use GPU if cuda is available
+        if cuda_is_available:  # put on GPU if CUDA is available
             model = model.cuda()
 
         model.apply(init_weights)
@@ -258,19 +243,5 @@ def main(mode):
         train(model, start)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main(sys.argv[1])
-        
-
-
-
-
-        
-
-
-
-
-
-
-
-
